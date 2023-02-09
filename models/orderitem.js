afde0552ea32ff3422afa17d72011fcc56e88b61
rivaldoseptian <rivaldo.septian121@gmail.com>
2023-02-09 12:06:37 +0700

    data

---
+++ models/orderitem.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class OrderItem extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      OrderItem.belongsToMany(models.User, { through: models.Order});
+      OrderItem.belongsToMany(models.Store, { through: models.Product});
+    }
+  }
+  OrderItem.init({
+    quantity: DataTypes.INTEGER,
+    price: DataTypes.INTEGER,
+    PruductId: DataTypes.INTEGER,
+    OrderId : DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'OrderItem',
+  });
+  return OrderItem;
+};
---
+++ models/user.js
@@ -0,0 +1,30 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      User.hasOne(models.UserDetail)
+      User.hasOne(models.Store)
+      User.belongsToMany(models.OrderItem, { through: models.Order});
+      
+    }
+  }
+  User.init({
+    userName: DataTypes.STRING,
+    password: DataTypes.STRING,
+    email: DataTypes.STRING,
+    role: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
